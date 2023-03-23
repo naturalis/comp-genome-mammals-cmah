350d354bf4f5f7bd756ece2e62d43a9a4d7d7820
YmkeNederlof <124789205+YmkeNederlof@users.noreply.github.com>
2023-03-23 13:20:37 +0100

    Delete download_genomes_script.R

--- download_genomes_script.R
+++
@@ -1,3 +0,0 @@
-curl -OJX GET "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCA_027475565.2/download?include_annotation_type=GENOME_FASTA,GENOME_GFF,RNA_FASTA,CDS_FASTA,PROT_FASTA,SEQUENCE_REPORT&filename=GCA_027475565.2.zip" -H "Accept: application/zip"
-#curl -OJX get "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCA_027475565.2/download?include_annotation_type=GENOME_FASTA,GENOME_GFF,RNA_FASTA,CDS_FASTA,PROT_FASTA,SEQUENCE_REPORT&filename=GCA_027475565.2.zip" -H "Accept: application/zip"
-
